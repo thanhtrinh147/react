b3a6462c142c0b7b681f620bdccf8cd495fa9011
Thành Trịnh <trinhvanthanh@vietbando.vn>
2023-02-17 17:44:44 +0700

    first commit

---
+++ my-app/src/components/tictactoe/Board.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Cell from './Cell';
+import './GameStyles.css'
+
+const Board  =(props) =>{
+    return <div className='game-board'>
+       {
+       props.cells.map((item, index)=>(
+            <Cell key={index} value={item} onClick={()=> props.onClick(index)}></Cell>
+        ))}
+
+    </div>;
+}
+
+export default Board;
---
+++ my-app/src/components/tictactoe/Game.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Board from './Board';
+import './GameStyles.css'
+
+const Game  =() =>{
+
+    
+    const [board, setBoard]= useState(Array(9).fill(null));
+    const handleClick=()=>{};
+
+    return (<div>
+        <Board cells={board} onClick={handleClick}></Board>
+    </div>);
+}
+
+export default Game;
